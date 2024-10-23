5eb43023bdaada3810ae97ba61faa28d9dc9b4c9
violetc <58360096+s-yh-china@users.noreply.github.com>
2024-10-23 14:59:44 +0800

    feat: 增加了添加账号引导

--- src/components/AddAccountDialog.vue
+++ src/components/AddAccountDialog.vue
@@ -1,52 +1,100 @@
 <script setup lang="ts">
-import { useDialogPluginComponent } from 'quasar';
+import { useDialogPluginComponent, useQuasar } from 'quasar';
 import { ref } from 'vue';
 import { api } from 'boot/axios';
 
 defineEmits([...useDialogPluginComponent.emits]);
 const { dialogRef, onDialogHide, onDialogOK } = useDialogPluginComponent();
 
+const $q = useQuasar();
+
+const loading = ref(false);
 const tokenValid = ref(true);
 
 const account = ref({
-  channel: 'BILIBILI',
+  channel: 'AUTO',
   token: ''
 });
 
-async function createAccount() {
-  if (account.value.token === '') {
+async function onConfirmClick() {
+  loading.value = true;
+  if (account.value.channel === 'AUTO') {
+    await foundToken();
+  } else {
+    await createAccount();
+  }
+  loading.value = false;
+}
+
+async function foundToken() {
+  try {
+    let tokenData = account.value.token;
+    try {
+      tokenData = JSON.parse(tokenData).data?.content || tokenData;
+    } catch {
+    }
+
+    if (tokenData.length === 24) {
+      account.value.channel = 'OFFICIAL';
+    } else if (tokenData.length === 296) {
+      account.value.channel = 'BILIBILI';
+    } else {
+      account.value.token = '';
+      tokenValid.value = false;
+      return;
+    }
+
+    account.value.token = tokenData;
+  } catch {
+    account.value.token = '';
     tokenValid.value = false;
     return;
   }
 
-  api.post('/accounts/create', {
-    channel: account.value.channel,
-    token: account.value.token
-  }).then((res) => {
+  await createAccount();
+}
+
+async function createAccount() {
+  if (account.value.token === '') {
+    return; // 就当没看见
+  }
+
+  try {
+    const res = await api.post('/accounts/create', {
+      channel: account.value.channel,
+      token: account.value.token
+    });
+
     if (res.status === 200) {
-      addToUser();
+      await addToUser();
     } else {
       tokenValid.value = false;
     }
-  }).catch(() => {
+  } catch {
     tokenValid.value = false;
-  });
+  }
 }
 
-function addToUser() {
-  api.post('/accounts/add_to_user', {
-    channel: account.value.channel,
-    token: account.value.token
-  }).then((res) => {
+async function addToUser() {
+  try {
+    const res = await api.post('/accounts/add_to_user', {
+      channel: account.value.channel,
+      token: account.value.token
+    });
+
     if (res.status === 200) {
+      $q.notify({
+        type: 'positive',
+        message: '添加成功'
+      });
       dialogRef.value?.hide();
       onDialogOK();
     } else {
       tokenValid.value = false;
     }
-  }).catch(() => {
+  } catch {
     tokenValid.value = false;
-  });
+  }
 }
 
 </script>
@@ -56,7 +104,7 @@ function addToUser() {
     <q-card class="w-[320px]">
       <q-card-section class="text-primary">
         <div class="text-h6">添加账号</div>
-        <q-input v-model="account.token" label="Token" />
+        <q-input v-model="account.token" :label="account.channel === 'AUTO' ? 'Context' : 'Token'" />
         <q-item-label v-if="!tokenValid" class="pt-2 text-red-400" caption>Token无效！</q-item-label>
       </q-card-section>
 
@@ -67,11 +115,15 @@ function addToUser() {
                       flat
                       text-color="grey-8"
                       :options="[
+                        {label: 'AUTO', value: 'AUTO'},
                         {label: 'B服', value: 'BILIBILI'},
                         {label: '官服', value: 'OFFICIAL'},
                       ]"
                       label="可用" />
-        <q-btn @click="createAccount" flat>确定</q-btn>
+        <q-btn @click="onConfirmClick" flat>
+          确定
+          <q-spinner v-if="loading" size="20px" class="q-ml-sm" />
+        </q-btn>
       </q-card-actions>
     </q-card>
   </q-dialog>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,7 +1,90 @@
 <template>
-  <q-page class="row items-center justify-evenly">
+  <q-page class="row items-center justify-evenly q-gutter-md">
+    <div id="card-groups" class="row q-gutter-md">
+      <div class="col-sm-12 col-lg-4">
+        <q-card class="card card-elevated">
+          <q-card-section>
+            <div class="card-body">
+              <h4>第 一 步</h4>
+              <div>在
+                <q-btn flat label="官网" class="custom-btn small-btn" href="https://ak.hypergryph.com/user" target="_blank"></q-btn>
+                检查登录状态
+              </div>
+            </div>
+          </q-card-section>
+        </q-card>
+      </div>
+      <div class="col-sm-12 col-lg-4">
+        <q-card class="card card-elevated">
+          <q-card-section>
+            <div class="card-body">
+              <h4>第 二 步</h4>
+              <div>复制所属服务器链接内 <strong class="text-red">全部内容</strong></div>
+              <div class="btn-group">
+                <q-btn flat label="官服" class="custom-btn small-btn" href="https://web-api.hypergryph.com/account/info/hg" target="_blank"></q-btn>
+                <q-btn flat label="B服" class="custom-btn-pink small-btn" href="https://web-api.hypergryph.com/account/info/ak-b" target="_blank"></q-btn>
+              </div>
+            </div>
+          </q-card-section>
+        </q-card>
+      </div>
+    </div>
   </q-page>
 </template>
 
+// TODO 有时间重写了比较好
+
 <script setup lang="ts">
 </script>
+
+<style scoped>
+.card {
+  height: 150px;
+  border-radius: 15px;
+}
+
+.card-elevated {
+  box-shadow: 0 4px 10px rgba(0, 0, 0, 0.2);
+  transition: transform 0.3s;
+}
+
+.card-elevated:hover {
+  transform: translateY(-5px);
+}
+
+.custom-btn {
+  background-color: #6200ea;
+  color: white;
+  border-radius: 20px;
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+  transition: background-color 0.3s, transform 0.3s;
+}
+
+.custom-btn:hover {
+  background-color: #3700b3;
+  transform: scale(1.05);
+}
+
+.custom-btn-pink {
+  background-color: #f86c96;
+  color: white;
+  border-radius: 20px;
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+  transition: background-color 0.3s, transform 0.3s;
+}
+
+.custom-btn-pink:hover {
+  background-color: #d81b60;
+  transform: scale(1.05);
+}
+
+.small-btn {
+  padding: 5px 10px;
+  font-size: 12px;
+}
+
+.btn-group {
+  display: flex;
+  gap: 10px;
+}
+</style>
